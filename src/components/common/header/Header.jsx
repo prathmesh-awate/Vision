ed9114732b2470c5899bf47a35c820b5d8d70dd8
prathmesh-awate <prathm235@gmail.com>
2023-09-22 18:06:57 +0530

    Tailwind Setup for Vite

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,12 +1,100 @@
-import React from 'react'
-import './Header.css'
-const Header = () => {
+import React, { useState } from 'react';
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
+    // <header className="bg-white">
+    //   <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    //     <div className="flex lg:flex-1">
+    //       <a href="#" className="-m-1.5 p-1.5">
+    //         <span className="sr-only">Your Company</span>
+    //         <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+    //       </a>
+    //     </div>
+    //     <div className="flex lg:hidden">
+    //       <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={toggleMenu}>
+    //         <span className="sr-only">Open main menu</span>
+    //         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+    //           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    //         </svg>
+    //       </button>
+    //     </div>
+    //     <div className={`lg:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
+    //       {/* Mobile menu */}
+    //       <div className="fixed inset-0 z-10"></div>
+    //       <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+    //         <div className="flex items-center justify-between">
+    //           <a href="#" className="-m-1.5 p-1.5">
+    //             <span className="sr-only">Your Company</span>
+    //             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+    //           </a>
+    //           <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={toggleMenu}>
+    //             <span className="sr-only">Close menu</span>
+    //             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+    //               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+    //             </svg>
+    //           </button>
+    //         </div>
+    //         <div className="mt-6 flow-root">
+    //           <div className="-my-6 divide-y divide-gray-500/10">
+    //             <div className="space-y-2 py-6">
+    //               <div className="-mx-3">
+    //                 <button type="button" className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+    //                   Product
+    //                   <svg className="h-5 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    //                     <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+    //                   </svg>
+    //                 </button>
+    //                 {/* 'Product' sub-menu */}
+    //                 <div className="mt-2 space-y-2">
+    //                   <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Analytics</a>
+    //                   <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Engagement</a>
+    //                   <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Security</a>
+    //                   <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Integrations</a>
+    //                   <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Automations</a>
+    //                   <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Watch demo</a>
+    //                   <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact sales</a>
+    //                 </div>
+    //               </div>
+    //               <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
+    //               <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
+    //               <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+    //             </div>
+    //             <div className="py-6">
+    //               <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className={`hidden lg:flex lg:gap-x-12 ${isMenuOpen ? 'hidden' : 'block'}`}>
+    //       {/* Desktop menu */}
+    //       <div className="relative">
+    //         <button type="button" className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
+    //           Product
+    //           <svg className="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    //             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+    //           </svg>
+    //         </button>
+    //         {/* ... (rest of the desktop menu content, omitted for brevity) */}
+    //       </div>
+    //     </div>
+    //     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+    //       <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+    //     </div>
+    //   </nav>
+    // </header>
     <>
-        <div>Header
-        </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
