38e714544875e71643bdba7b8bc506c089bcc346
prathmesh-awate <prathm235@gmail.com>
2023-10-30 02:10:19 +0530

    logo added

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -16,12 +16,12 @@ function Header() {
   return (
   <header className="bg-white">
   <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-    <div className="flex lg:flex-1">
+  <div className="flex lg:flex-1">
       <a href="#" className="-m-1.5 p-1.5">
-        <span className="sr-only">Your Company</span>
-        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+        <span className="sr-only">Vision</span>
+        <img className="h-30 w-auto" src="/vite.svg" alt="" />
       </a>
-    </div>
+</div>
     
     <div className="flex lg:hidden">
       
@@ -194,7 +194,6 @@ function Header() {
     </div>
   </div>
 </header>
-
   );
 }
 
